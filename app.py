4b2fb3cf8905c48f70b105dad6231fc7daa1311e
Patrick Russell <prussell94@gmail.com>
2023-08-06 17:53:49 -0400

    add session such that data can be passed from different templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import psycopg2
@@ -60,41 +60,46 @@ def index():
     # close the cursor and connection
     cur.close()
     conn.close()
+    session['answer_data'] = data
+    # return Flask.redirect(Flask.url_for('submit'))
     return render_template('index.html', data=data)
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Connect to the database
-    conn = psycopg2.connect(database="test_db",
-                            user="root",
-                            password="root",
-                            host="localhost", port="5432")
+    # # Connect to the database
+    # conn = psycopg2.connect(database="test_db",
+    #                         user="root",
+    #                         password="root",
+    #                         host="localhost", port="5432")
   
-    # create a cursor
-    cur = conn.cursor()
+    # # create a cursor
+    # cur = conn.cursor()
   
-    # Select all products from the table
-    cur.execute('''SELECT
-	* FROM clues OFFSET floor(random() * (
-		SELECT
-			COUNT(*)
-			FROM clues))
-    LIMIT 1;''')
+    # # Select all products from the table
+    # cur.execute('''SELECT
+	# * FROM clues OFFSET floor(random() * (
+	# 	SELECT
+	# 		COUNT(*)
+	# 		FROM clues))
+    # LIMIT 1;''')
   
-    # Fetch the data
-    data = cur.fetchall()
-    print(data[-1][-1])
+    # # Fetch the data
+    # data = cur.fetchall()
+    # print(data[0][-1])
   
-    # close the cursor and connection
-    cur.close()
-    conn.close()
+    # # close the cursor and connection
+    # cur.close()
+    # conn.close()
+    data = session.get("answer_data",None)
     if request.method == 'POST':
         answer= request.form['answer']
-        print(answer == data)
-        if(answer == data):
-            return render_template("index.html", message='Correct answer!', data=data)
+        correctAnswer = data[0][-1]
+        if(answer == correctAnswer):
+            return render_template("index.html", message='Correct!', data=data)
         else:
-            return render_template("index.html", message='Incorrect answer!', data=data)
+            return render_template("index.html", message='Incorrect!', data=data)
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run()
